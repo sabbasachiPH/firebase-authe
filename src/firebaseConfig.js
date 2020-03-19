49d2c8d49b24d275e93688f7f621a9feffa6f987
sabbasachiPH <rentustat02@gmail.com>
2020-03-19 08:52:18 +0600

    commit after lecture no 26.8

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBqmu3frFkLaI10VHxJI6Sgfb8pee1XXhM",
+    authDomain: "sab-ema-john-simple.firebaseapp.com",
+    databaseURL: "https://sab-ema-john-simple.firebaseio.com",
+    projectId: "sab-ema-john-simple",
+    storageBucket: "sab-ema-john-simple.appspot.com",
+    messagingSenderId: "271639549214",
+    appId: "1:271639549214:web:f18baaf7805645929fa50f"
+  };
+  export default firebaseConfig;
